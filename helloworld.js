10cdb6131c8e569c46af43b4f2c9a6b5ce6ae6e6
stlbpw <69367908+stlbpw@users.noreply.github.com>
2024-09-22 19:06:21 -0500

    Update helloworld.js

--- helloworld.js
+++ helloworld.js
@@ -1,4 +1,4 @@
 // Console will print the message
-console.log('Hello World From Bruce - Auto\n')
+console.log('\n\nHello World From Bruce - Auto\n')
 console.log('Test Commit 11\n')
 process.exit(0);
